2168a21b80ffea482f388b7195d0b43f24750035
ma-jiale <majialea@126.com>
2024-12-16 10:49:43 +0800

    新增Unity工程文件

---
+++ Software/HeartMelody/Assets/_Scripts/Interaction.cs
@@ -0,0 +1,137 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Interaction : MonoBehaviour
+{
+    public void go_to_Welcome()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("Welcome");    
+        
+    }
+    public void go_to_UsePhoneNumber()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("UsePhoneNumber");    
+        
+    }
+
+    public void go_to_SignIn()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("SignIn");    
+        
+    }
+
+        public void go_to_Home()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("Home");    
+        
+    }
+
+    public void go_to_Explore()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("Explore");    
+        
+    }
+
+    public void go_to_MoodSquare()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("MoodSquare");    
+        
+    }
+
+    public void go_to_MySpace()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("MySpace");    
+        
+    }
+
+    public void go_to_Muyu()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("Muyu");    
+        
+    }
+
+    public void go_to_Fish()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("Fish");    
+        
+    }
+
+    public void go_to_Flower()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("Flower");    
+        
+    }
+
+    public void go_to_AddMood()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("AddMood");    
+        
+    }
+
+        public void go_to_MoodHistory()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("MoodHistory");    
+        
+    }
+
+    public void go_to_Setting()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("Setting");    
+        
+    }
+
+    public void go_to_MyInfo()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("MyInfo");    
+        
+    }
+
+    public void go_to_Projects()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("Projects");    
+        
+    }
+
+    public void go_to_History()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("History");    
+        
+    }
+
+    public void go_to_Favourite()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("Favourite");    
+        
+    }
+
+    public void go_to_Message()
+    {
+        // 加载新场景
+        SceneManager.LoadScene("Message");    
+        
+    }
+
+
+
+
+
+
+
+}
